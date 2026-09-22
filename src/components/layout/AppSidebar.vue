75d3a127cb2bd0fe5db4acb5b8a68e29f83ae4ad
RafaBonk <rafabonk223@gmail.com>
2026-09-22 09:15:08 -0300

    first commit

---
+++ src/components/layout/AppSidebar.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import {RouterLink} from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <div class="flex-column nav-pills gap-1">
+      <RouterLink to=/Pets>
+      <i class="bi bi-heart me-2"></i>
+      Pets novo
+      </RouterLink>
+    </div>
+
+  </nav>
+
+</template>
---
+++ src/components/layout/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import {RouterView} from "vue-router";
+  import AppNavbar from "/@components/layout/AppNavbar.vue";
+  import AppSidebar from "/@components/layout/AppSidebar.vue";
+</script>
+
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <aside class="col-md-3"> </aside>
+    </div>
+  </div>
+</template>
+
+
+
